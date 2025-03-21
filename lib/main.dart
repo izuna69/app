2e6e32166e8baae139351c732f692daf7087fd79
izuna69 <94025204+izuna69@users.noreply.github.com>
2025-03-21 16:28:47 +0900

    20250321 dart의 기본구조배우기

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,44 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('my app'),
       ),
-      body: const Center(
-        child: Text(
-          'Hello, World!',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+      body: Stack(
+        children: <Widget>[
+          const Center(
+            child: Text(
+              'hello',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 0,
+            right: 0,
+            child: ElevatedButton(
+              onLongPress: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('버튼 클릭됨'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('닫기'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              onPressed: () {
+                // 버튼 클릭 시의 행동을 정의할 수 있음 (현재는 비어 있음)
+              },
+              child: const Text('버튼 클릭'),
+            ),
+          ),
+        ],
       ),
     );
   }
